7c35bd6e2fc91fd33e3cf3bda480c50bd7574b57
nlacerdagabriel <devlacerdagabriel@hotmail.com>
2023-11-06 14:29:47 -0300

    first commit

---
+++ start/routes.ts
@@ -0,0 +1,26 @@
+
+
+import Route from '@ioc:Adonis/Core/Route'
+
+Route.get('/', async () => {
+  return { hello: 'world' }
+})
+
+Route.group(() => {
+  Route.post('/create', 'CustomWordController.store')
+  Route.get('/get-all', 'CustomWordController.getAll')
+  Route.get('/get-one/:id', 'CustomWordController.getOne')
+  Route.put('/update-one/:id', 'CustomWordController.updateOne')
+  Route.delete('/delete-one/:id', 'CustomWordController.deleteOne')
+}).prefix('/custom-word').middleware("auth");
+
+Route.group(() => {
+  Route.post("/login", "AuthController.login");
+  Route.post("/logout", "AuthController.logout").middleware("auth");
+}).prefix("/auth");
+
+Route.group(() => {
+  Route.get("/:key", "S3sController.getObjectUrl");
+}).prefix("/s3");
+
+Route.post('/converter', 'ConversionsController.convert');
